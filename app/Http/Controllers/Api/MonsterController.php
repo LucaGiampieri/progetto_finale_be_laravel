b822e40a1fb1c6ef565cafaa9661bf47dbc0d1fd
Luca Giampieri <luca.giampieri95@gmail.com>
2026-05-19 20:27:14 +0200

    Aggiunta Api di index e di show per Monsters

---
+++ app/Http/Controllers/Api/MonsterController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Monster;
+use Illuminate\Http\Request;
+
+class MonsterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $monsters = Monster::with('types', 'size')->get();
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $monsters
+            ]
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Monster $monster)
+    {
+        $monster->load('types', 'size');
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $monster
+            ]
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\MonsterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('monsters', [MonsterController::class, 'index']);
+
+Route::get('monsters/{monster}', [MonsterController::class, 'show']);
